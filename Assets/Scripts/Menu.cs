e449fb341d1fcfd383e76471658cd8969456deb9
Catta <catalincatta@yahoo.com>
2023-07-16 05:53:48 +0300

    added functionalities to menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;  
+
+public class Menu : MonoBehaviour
+{
+    
+    public void StartEndlessRun() =>
+        SceneManager.LoadScene("EndlessRun");
+    
+    public void Quit() =>
+        Application.Quit();  
+    
+    public void StartLevel(int lvl) =>  
+        SceneManager.LoadScene($"Lvl {lvl}");  
+
+}
---
+++ Assets/Scripts/MenuLight.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuLight : MonoBehaviour
+{
+    private bool _darkMode;
+    
+    [SerializeField] private Sprite darkSun;
+    [SerializeField] private Sprite ligthSun;
+    
+    public void ChangeTheme()
+    {
+        _darkMode = !_darkMode;
+        
+        transform.parent.parent.GetComponent<Image>().color = _darkMode ? new Color(0.1f, 0.1f, 0.3f) : Color.cyan;
+        transform.parent.GetComponent<Image>().color = _darkMode ? new Color(0.5f, 1f, 0.8f, 0.5f) : new Color(0f, 1f, 0.5f, 1f);
+
+        var selfImage = transform.GetComponent<Image>();
+
+        selfImage.color = _darkMode ? new Color(0.4f, 0.4f, 1) : Color.white;
+        selfImage.sprite = _darkMode ? darkSun : ligthSun;
+
+        transform.GetChild(0).gameObject.SetActive(_darkMode);
+    }
+}
