4c174cc86d375411514f98a3f636f1ca64c15b17
Catta <catalincatta@yahoo.com>
2023-07-19 13:59:52 +0300

    created settings display

---
+++ Assets/Scripts/Manager/SettingsManager.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class SettingsManager: MonoBehaviour
+{
+    public static SettingsManager Instance;
+    
+    public float generalVolume = 1f;
+    public float musicVolume = 0.5f;
+    public float soundEffectVolume = 1f;
+    public bool soundMuted;
+    
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
